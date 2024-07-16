aafdafef33b676b2ee19b722a027de7c1f5565ca
PhDBui <congdoan6997@gmail.com>
2024-07-17 00:54:14 +0300

    feat: 🎸 empty states

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+
+import { EmptySearch } from "./empty-search";
+import { EmptyBoards } from "./empty-boards";
+import { EmptyFavorites } from "./empty-favorites";
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+//   const data = useQuery(api.boards.get, {
+//     orgId,
+//     ...query,
+//   });
+  return <EmptyFavorites />;
+};
