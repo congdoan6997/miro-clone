92f7dbab316db3ccfa897fd7561d392fc5cdc563
PhDBui <congdoan6997@gmail.com>
2024-07-17 00:18:15 +0300

    feat: 🎸 organization sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
 import { PropsWithChildren } from "react";
 import Sidebar from "./_components/sidebar";
-
-
+import { OrgSidebar } from "./_components/org-sidebar";
 
 const DashboardLayout = ({ children }: PropsWithChildren) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">{children}</div>
+        </div>
+      </div>
     </main>
   );
 };
